d967d2cd7da83880ee54c5fdbe1935bcb279d802
data-alch3m1st <d4t4.4lch3my+g1thub@gmail.com>
2025-09-19 15:00:23 -0400

    Update crypto_helper_scripts.py

--- crypto_helper_scripts/crypto_helper_scripts.py
+++ crypto_helper_scripts/crypto_helper_scripts.py
@@ -1,2 +1,18 @@
 # crypto_helper_scripts.py #
 
+class OsintHelper:
+    
+    """
+    _summary_
+    A class to encapsulate helper functions specifically for OSINT'ing ;)
+    """
+    
+    @staticmethod
+    def defang_url(url):
+        """
+        Function to 'defang' a URL to prevent it from being clickable to prevent accidental OPSEC challenges.
+        
+        Args:
+            url (_type_): any url (with or without 'http/https')
+        """
+        
